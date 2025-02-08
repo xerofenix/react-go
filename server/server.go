cafa7d3f78f62a698712983a381dcf15fa12faf5
xerofenix <xerofenix@duck.com>
2025-02-08 17:45:55 +0530

    database connection completed with automigration

--- server/server.go
+++ server/server.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/xerofenix/react-go/database"
+)
+
+func init() {
+	database.LoadEnv()
+	database.ConnectDB()
+
+}
 
 func main() {
+
+	sqlDb, err := database.DBCon.DB()
+	if err != nil {
+		println("Error while connecting Databse", err)
+	}
+	defer sqlDb.Close()
+
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello world",
-		})
+		return c.SendFile("index.html", false)
 	})
 
 	app.Listen(":8008")
